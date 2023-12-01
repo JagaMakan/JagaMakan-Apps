a8226ebbc2b1a9e9de344c46d054518851672cd3
MazpissVisual <Mazpissiqbal@gmail.com>
2023-12-01 20:37:04 +0700

    first commit

---
+++ Mobile_Dev/app/src/main/java/com/mazpiss/jagamakan/ui/register/RegisterActvity.kt
@@ -0,0 +1,112 @@
+package com.mazpiss.jagamakan.ui.register
+
+import android.content.Intent
+import android.os.Bundle
+import android.content.Context
+import android.net.ConnectivityManager
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.FirebaseAuthUserCollisionException
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException
+import com.mazpiss.jagamakan.ui.main.MainActivity
+import com.mazpiss.jagamakan.databinding.ActivityRegisterBinding
+import com.mazpiss.jagamakan.ui.login.LoginActivity
+
+class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.button.setOnClickListener {
+            if (isNetworkAvailable()) {
+                val email = binding.emailEt.text.toString()
+                val pass = binding.passET.text.toString()
+                val confirmPass = binding.confirmPassEt.text.toString()
+
+                if (email.isNotEmpty() && pass.isNotEmpty() && confirmPass.isNotEmpty()) {
+                    if (pass == confirmPass) {
+                        if (pass.length > 7) { // Check if password is longer than 7 characters
+                            firebaseAuth.createUserWithEmailAndPassword(email, pass)
+                                .addOnCompleteListener { task ->
+                                    if (task.isSuccessful) {
+                                        // Show success dialog
+                                        showSuccessDialog("Kamu berhasil daftar! \nsilahkan masuk")
+                                    } else {
+                                        // Check if it's an authentication issue
+                                        if (task.exception is FirebaseAuthWeakPasswordException) {
+                                            // Show error dialog for weak password
+                                            showErrorDialog("Password is too weak. Please choose a stronger password.")
+                                        } else if (task.exception is FirebaseAuthInvalidCredentialsException) {
+                                            // Show error dialog for invalid email
+                                            showErrorDialog("Invalid email format.")
+                                        } else if (task.exception is FirebaseAuthUserCollisionException) {
+                                            // Show error dialog for existing user
+                                            showErrorDialog("Account already exists with this email.")
+                                        } else {
+                                            // Show a generic error dialog
+                                            showErrorDialog(task.exception?.message ?: "Registration failed")
+                                        }
+                                    }
+                                }
+                        } else {
+                            // Show error dialog for short password
+                            showErrorDialog("Password must be at least 8 characters long.")
+                        }
+                    } else {
+                        showErrorDialog("Password is not matching")
+                    }
+                } else {
+                    showErrorDialog("Empty Fields Are not Allowed !!")
+                }
+            } else {
+                showNoInternetDialog()
+            }
+        }
+    }
+
+    private fun showErrorDialog(message: String) {
+        val alertDialog = AlertDialog.Builder(this)
+        alertDialog.setTitle("Error")
+            .setMessage(message)
+            .setPositiveButton("OK", null)
+            .setCancelable(true)
+            .show()
+    }
+
+    private fun showSuccessDialog(message: String) {
+        val alertDialog = AlertDialog.Builder(this)
+        alertDialog.setTitle("Success")
+            .setMessage(message)
+            .setPositiveButton("OK") { dialog, which ->
+                // Go to MainActivity
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+            .setCancelable(false)
+            .show()
+    }
+
+    private fun showNoInternetDialog() {
+        val alertDialog = AlertDialog.Builder(this)
+        alertDialog.setTitle("No Internet Connection")
+            .setMessage("Ups, sepertinya koneksi internetmu terputus. Silakan cek kembali.")
+            .setPositiveButton("OK", null)
+            .setCancelable(true)
+            .show()
+    }
+
+    private fun isNetworkAvailable(): Boolean {
+        val connectivityManager =
+            getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetworkInfo = connectivityManager.activeNetworkInfo
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected
+    }
+}
